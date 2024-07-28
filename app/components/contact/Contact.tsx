b5e33f1296c47cb917bd568eb0d902c70dfdb996
moses mwangi <moses.mwangi.me@gmail.com>
2024-07-28 20:24:48 +0300

    commit

--- app/components/contact/Contact.tsx
+++ app/components/contact/Contact.tsx
@@ -14,12 +14,11 @@ const play = Playfair_Display({
 
 export default function Contact() {
   return (
-    <div
-      id="contact"
-      className="px-6 md:w-[80%] mx-auto mb-32 pt-36"
-      data-aos="fade-up"
-    >
-      <div className="flex gap-3 items-center justify-center mb-10 md:mb-16">
+    <div id="contact" className="px-6 md:w-[80%] mx-auto mb-32 pt-36">
+      <div
+        data-aos="fade-up"
+        className="flex gap-3 items-center justify-center mb-10 md:mb-16"
+      >
         <span className=" h-[1px] w-[200px] bg-gray-700 block" />
         <p className="text-2xl md:text-4xl font-[600] whitespace-nowrap">
           <span
@@ -34,8 +33,8 @@ export default function Contact() {
         <span className=" h-[1px] w-[200px] bg-gray-700 block" />
       </div>
       <div
-        className=" grid md:grid-cols-[1fr_1.4fr] grid-cols-1 gap-6 items-center"
         data-aos="fade-up"
+        className=" grid md:grid-cols-[1fr_1.4fr] grid-cols-1 gap-6 items-center"
       >
         <div>
           <ContactForm />
